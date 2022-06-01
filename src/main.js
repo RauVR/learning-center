8011cf3e24deafc05722738516f6a66c22072167
Raul Ventura <u201714049@upc.edu.pe>
2022-06-01 18:15:24 -0500

    chore(ui): Added PrimeVue Theme configuration.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
 
-const app = createApp(App)
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primevue/resources/themes/md-light-indigo/theme.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+app.use(PrimeVue);
+
+app.mount("#app");
